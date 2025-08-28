06316e0aa1542b8eefa0fca361f0beaa743fbd49
HoangHuy2501 <huy04.developer@gmail.com>
2025-08-28 17:15:39 +0700

    deloy 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: "0.0.0.0",
+    port:  3000,
+    allowedHosts: ["notesummerize.onrender.com"], // cho phép domain Render
+  },
+});
