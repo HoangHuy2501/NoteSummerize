304e87defcfadea0c8709626909ffbfb746ecd70
HoangHuy2501 <huy04.developer@gmail.com>
2025-09-04 02:15:14 +0700

    static 5

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Base URL của app, thay đổi nếu deploy ở subpath
   preview: {
     host: "0.0.0.0",
-    port:  3000,
-    allowedHosts: ["notesummerize.onrender.com"], // cho phép domain Render
+    port: 3000,
+    allowedHosts: ["notesummerize.onrender.com"],
   },
 });
